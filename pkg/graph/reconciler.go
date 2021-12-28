4291a5342279c4000ad6340e62938b4bbc29a658
Tamal Saha <tamal@appscode.com>
2021-12-28 00:40:09 -0800

    Use new context for Get calls inside reconciler
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/graph/reconciler.go
+++ pkg/graph/reconciler.go
@@ -42,7 +42,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, req reconcile.Request) (reco
 
 	var obj unstructured.Unstructured
 	obj.SetGroupVersionKind(gvk)
-	if err := r.Get(ctx, req.NamespacedName, &obj); err != nil {
+	if err := r.Get(context.TODO(), req.NamespacedName, &obj); err != nil {
 		log.Error(err, "unable to fetch", "group", r.R.Group, "kind", r.R.Kind)
 		// we'll ignore not-found errors, since they can't be fixed by an immediate
 		// requeue (we'll need to wait for a new notification), and we can get them
