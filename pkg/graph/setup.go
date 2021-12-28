d092376303aa4a7977f7b98dde6da6f038b823df
Tamal Saha <tamal@appscode.com>
2021-12-28 04:25:29 -0800

    Ignore not found errors from query results
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/graph/setup.go
+++ pkg/graph/setup.go
@@ -156,10 +156,11 @@ func ExecQuery(c client.Client, query string, vars map[string]interface{}) ([]un
 		var obj unstructured.Unstructured
 		obj.SetGroupVersionKind(mapping.GroupVersionKind)
 		err = c.Get(context.TODO(), client.ObjectKey{Namespace: ref.Namespace, Name: ref.Name}, &obj)
-		if err != nil {
+		if client.IgnoreNotFound(err) != nil {
 			return nil, errors.Wrap(err, "failed to extract refs")
+		} else if err == nil {
+			objs = append(objs, obj)
 		}
-		objs = append(objs, obj)
 	}
 	return objs, nil
 }
