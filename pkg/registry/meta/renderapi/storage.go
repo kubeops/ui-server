93cc0c1a4dea051f47dbb89bf810d922239bbf47
Tamal Saha <tamal@appscode.com>
2022-03-01 13:36:47 -0800

    Support convertToTable in renderapi (#106)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/registry/meta/renderapi/storage.go
+++ pkg/registry/meta/renderapi/storage.go
@@ -27,6 +27,7 @@ import (
 	"k8s.io/apiserver/pkg/authorization/authorizer"
 	"k8s.io/apiserver/pkg/registry/rest"
 	rsapi "kmodules.xyz/resource-metadata/apis/meta/v1alpha1"
+	"kmodules.xyz/resource-metadata/pkg/tableconvertor"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -67,38 +68,51 @@ func (r *Storage) Create(ctx context.Context, obj runtime.Object, _ rest.Validat
 	}
 	req := in.Request
 
+	gvk := schema.GroupVersionKind{
+		Group:   req.Group,
+		Version: req.Version,
+		Kind:    req.Kind,
+	}
 	if req.Selector == nil {
 		var out unstructured.Unstructured
-		out.SetGroupVersionKind(schema.GroupVersionKind{
-			Group:   req.Resource.Group,
-			Version: req.Resource.Version,
-			Kind:    req.Resource.Kind,
-		})
-		err := r.kc.Get(context.TODO(), client.ObjectKey{Namespace: req.Ref.Namespace, Name: req.Ref.Name}, &out)
+		out.SetGroupVersionKind(gvk)
+		err := r.kc.Get(context.TODO(), client.ObjectKey{Namespace: req.Namespace, Name: req.Name}, &out)
 		if err != nil {
 			return nil, err
 		}
-		in.Response = &out
+		if req.ConvertToTable {
+			table, err := tableconvertor.TableForObject(r.kc, &out)
+			if err != nil {
+				return nil, err
+			}
+			in.Response = table
+		} else {
+			in.Response = &out
+		}
 	} else {
 		selector, err := metav1.LabelSelectorAsSelector(req.Selector)
 		if err != nil {
 			return nil, err
 		}
 		opts := client.ListOptions{
-			Namespace:     req.Ref.Namespace,
+			Namespace:     req.Namespace,
 			LabelSelector: selector,
 		}
 		var out unstructured.UnstructuredList
-		out.SetGroupVersionKind(schema.GroupVersionKind{
-			Group:   req.Resource.Group,
-			Version: req.Resource.Version,
-			Kind:    req.Resource.Kind,
-		})
+		out.SetGroupVersionKind(gvk)
 		err = r.kc.List(context.TODO(), &out, &opts)
 		if err != nil {
 			return nil, err
 		}
-		in.Response = &out
+		if req.ConvertToTable {
+			table, err := tableconvertor.TableForList(r.kc, out.Items)
+			if err != nil {
+				return nil, err
+			}
+			in.Response = table
+		} else {
+			in.Response = &out
+		}
 	}
 
 	return in, nil
