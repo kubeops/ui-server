41084720e185deaea9d9e372da5c41bc54bbd525
Tamal Saha <tamal@appscode.com>
2026-06-04 10:15:13 +0600

    Add NatsCredentialRequest extended API storage
    Wires a new natscredentialrequests resource under
    identity.k8s.appscode.com/v1alpha1 by delegating to the b3 identity client's
    GetNatsCredential method. Modelled on the existing inboxtokenrequest
    storage. Vendor bump is hand-applied and will be replaced by a normal
    go mod vendor once the resource-metadata change lands.
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ pkg/registry/identity/natscredentialrequest/storage.go
@@ -0,0 +1,90 @@
+/*
+Copyright AppsCode Inc. and Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package natscredentialrequest
+
+import (
+	"context"
+	"strings"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apiserver/pkg/registry/rest"
+	identityapi "kmodules.xyz/resource-metadata/apis/identity/v1alpha1"
+	identitylib "kmodules.xyz/resource-metadata/pkg/identity"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Storage struct {
+	kc client.Client
+	bc *identitylib.Client
+}
+
+var (
+	_ rest.GroupVersionKindProvider = &Storage{}
+	_ rest.Scoper                   = &Storage{}
+	_ rest.Storage                  = &Storage{}
+	_ rest.Creater                  = &Storage{}
+	_ rest.SingularNameProvider     = &Storage{}
+)
+
+func NewStorage(kc client.Client, bc *identitylib.Client) *Storage {
+	return &Storage{
+		kc: kc,
+		bc: bc,
+	}
+}
+
+func (r *Storage) GroupVersionKind(_ schema.GroupVersion) schema.GroupVersionKind {
+	return identityapi.SchemeGroupVersion.WithKind(identityapi.ResourceKindNatsCredentialRequest)
+}
+
+func (r *Storage) NamespaceScoped() bool {
+	return false
+}
+
+func (r *Storage) GetSingularName() string {
+	return strings.ToLower(identityapi.ResourceKindNatsCredentialRequest)
+}
+
+func (r *Storage) New() runtime.Object {
+	return &identityapi.NatsCredentialRequest{}
+}
+
+func (r *Storage) Destroy() {}
+
+func (r *Storage) Create(ctx context.Context, obj runtime.Object, _ rest.ValidateObjectFunc, _ *metav1.CreateOptions) (runtime.Object, error) {
+	req, ok := obj.(*identityapi.NatsCredentialRequest)
+	if !ok {
+		return nil, apierrors.NewBadRequest("expected NatsCredentialRequest")
+	}
+	if req.Request == nil {
+		return nil, apierrors.NewBadRequest("request is required")
+	}
+	if len(req.Request.License) == 0 {
+		return nil, apierrors.NewBadRequest("request.license is required")
+	}
+
+	resp, err := r.bc.GetNatsCredential(req.Request.Features, req.Request.License)
+	if err != nil {
+		return nil, err
+	}
+	req.Response = resp
+
+	return req, nil
+}
